bdca4f30863221c1f2114cf753abf9d303909954
ryanfulford <ryanfulford@fastmail.com>
2022-02-08 01:54:31 -0500

    tinkered with javascript

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-// JS Thiings to Try
+// JS Things to Try
 
 // 1. Give user an alert when form button is submitted
 // 2. Change button background on click
 // 3. Add some sort of effect on hambuger menu icon when clicked
-// 4. See if image carousel or image pop-up modals are possible
 
 
 // ***** 1. Provide alert when user submits form
 
 // target form button on blog and contact pages
+
 const form = document.querySelector('form');
 console.log(form);
 
 // add event listener for form sumbit button
+
 form.addEventListener('submit', function(event){
   event.preventDefault();
-  alert('Thank you for contacting us! We will be in touch as soon as possible.');
+  alert('Thank you for contacting us! We will be in touch as soon as possible.');  
 });
 
+// ***** 2. Change button background on click
 
-// ***** 2. Change button background colour when clicked
+// let clickButton = document.querySelector('button');
+// console.log(clickButton);
 
-// target buttons on website pages
+//   clickButton.addEventListener('click', function(){
+//   clickButton.style.backgroundColor = '#FA8128';
+//   });
 
 
 
