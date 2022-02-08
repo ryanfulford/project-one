8de6442761ec521b087f09436c29c50e0c52bfdd
ryanfulford <ryanfulford@fastmail.com>
2022-02-07 23:19:55 -0500

    added script file and added form button functionality

---
+++ script.js
@@ -0,0 +1,28 @@
+// JS Thiings to Try
+
+// 1. Give user an alert when form button is submitted
+// 2. Change button background on click
+// 3. Add some sort of effect on hambuger menu icon when clicked
+// 4. See if image carousel or image pop-up modals are possible
+
+
+// ***** 1. Provide alert when user submits form
+
+// target form button on blog and contact pages
+const form = document.querySelector('form');
+console.log(form);
+
+// add event listener for form sumbit button
+form.addEventListener('submit', function(event){
+  event.preventDefault();
+  alert('Thank you for contacting us! We will be in touch as soon as possible.');
+});
+
+
+// ***** 2. Change button background colour when clicked
+
+// target buttons on website pages
+
+
+
+
