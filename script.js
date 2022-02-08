41504339b08b8ec3c58188db93612342730a784e
ryanfulford <ryanfulford@fastmail.com>
2022-02-08 17:25:58 -0500

    added js functionality to buttons

--- script.js
+++ script.js
@@ -2,31 +2,36 @@
 
 // 1. Give user an alert when form button is submitted
 // 2. Change button background on click
-// 3. Add some sort of effect on hambuger menu icon when clicked
-
 
 // ***** 1. Provide alert when user submits form
 
 // target form button on blog and contact pages
+// const form = document.querySelector('form');
+// console.log(form);
 
-const form = document.querySelector('form');
-console.log(form);
+// // add event listener for form sumbit button
 
-// add event listener for form sumbit button
-
-form.addEventListener('submit', function(event){
-  event.preventDefault();
-  alert('Thank you for contacting us! We will be in touch as soon as possible.');  
-});
+// form.addEventListener('submit', function(event){
+//   event.preventDefault();
+//   alert('Thank you for contacting us! We will be in touch as soon as possible.');  
+// });
 
 // ***** 2. Change button background on click
 
-// let clickButton = document.querySelector('button');
-// console.log(clickButton);
+buttonChanger = function() {
+  const buttons = document.querySelectorAll('button');
 
-//   clickButton.addEventListener('click', function(){
-//   clickButton.style.backgroundColor = '#FA8128';
-//   });
+  buttons.forEach(function(individualButton) {
+    individualButton.addEventListener('click', function(){
+      const selectedButton = this.id;
+      individualButton.style.backgroundColor = '#FA8128';
+    });
+  });
+}
+
+buttonChanger();
+
+// Cannot get both form submit and button color change to work at the same time, form is null, investigate this to debug.
 
 
 
